53a43a1d4e26596e745f6ae584e73f60219b2afe
X-xom <71981066+X-xom@users.noreply.github.com>
2022-11-15 20:53:28 +0300

    Update ЛБ-3 №14 lvl1.cs

--- ЛБ-3 №14 lvl1.cs
+++ ЛБ-3 №14 lvl1.cs
@@ -1,37 +1,24 @@
+// Найти сумму квадратов элементов, расположенных до первого отрицательного элемента массива размера 11.
 using System;
 class HelloWorld {
-  static void Main() {
-        double[] a = new double[5] {4.0, 3.0, 4.0, -5.0, -7.0};
-        double s = 0;
-        foreach (double x in a)
-        {
-            if (x > 0){
-                s = s + x*x;
-        }
-        else
-        {
-            break;
-        }
-        }
-        Console.WriteLine(s);
-  }
-}
-
-
-using System;
-class HelloWorld {
-  static void Main() {
-    Console.WriteLine("Введите размер массива");
-    int n = int.Parse(Console.ReadLine());
-    double[] a = new double[n];
-    Console.WriteLine("Введите массив");
+    static void Main() {
+    int[] a = new int[11];
     string s = Console.ReadLine();
     string[] c = s.Split(' ');
-    for (double i = 0; i < n; i = i + 1)
+    for (int i = 0; i < 11; i = i + 1)
     {
-        a[i] = double.Parse(c[i]);
+        a[i] = int.Parse(c[i]);
     }
-    for (int i = 0; i < n; i++)
-        Console.Write("{0:d} ", a[i]);
+    double m = 0;
+    foreach (double x in a)
+    {
+        if (x >= 0){
+            m = m + x*x;
+    }
+        else{
+        break;
+    }
+    }
+    Console.WriteLine(m);
+    }
   }
-}
