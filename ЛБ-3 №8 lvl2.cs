da5249d3792425ea52ae2cb94ab9263f354be943
X-xom <71981066+X-xom@users.noreply.github.com>
2022-11-19 23:18:16 +0300

    Create ЛБ-3 №8 lvl2.cs

---
+++ ЛБ-3 №8 lvl2.cs
@@ -0,0 +1,32 @@
+//Поменять местами максимальный элемент массива и минимальный элемент части массива, расположенной после максимального.
+
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Введите размер массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите массив");
+    double[] a = new double[size];
+    string s = Console.ReadLine();
+    string[] c = s.Split(' ');
+    for (int i = 0; i < size; i = i + 1)
+    {
+        a[i] = double.Parse(c[i]);
+    }
+    double amax = a[0];
+    int imax = 0;
+    for (int i = 1; i < 5; i++){
+        if (a[i] >= amax){
+            amax = a[i];
+            imax = i;
+        }
+    }
+    double amin = a[imax];
+    for (int i = imax; i < 5; i++){
+        if (a[i] <= amin){
+            amin = a[i];
+        }
+    }
+    Console.WriteLine(amax + " " + amin);
+  }
+}
